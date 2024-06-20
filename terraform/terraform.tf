e827678079fea266ab9d6e232beb1489868433ba
David Yeo <david.j.yeo@outlook.com>
2024-06-20 09:38:07 +0100

    strip back

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -50,10 +50,10 @@ provider "azurerm" {
     }
   }
 
-  subscription_id     = var.AZURE_SUBSCRIPTION_ID
-  client_id           = var.AZURE_CLIENT_ID
-  client_secret       = var.AZURE_CLIENT_SECRET
-  tenant_id           = var.AZURE_TENANT_ID
+  # subscription_id     = var.AZURE_SUBSCRIPTION_ID
+  # client_id           = var.AZURE_CLIENT_ID
+  # client_secret       = var.AZURE_CLIENT_SECRET
+  # tenant_id           = var.AZURE_TENANT_ID
   storage_use_azuread = true
 }
 

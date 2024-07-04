52b71d518d625f0eea4d93b90a6898ecb6715f8d
David Yeo <david.j.yeo@outlook.com>
2024-07-04 08:13:48 +0100

    build: tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,11 @@ resource "azurerm_user_assigned_identity" "uai" {
   location            = azurerm_resource_group.rg.location
   name                = module.naming.user_assigned_identity.name
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Service     = "Ansible"
+    Environment = "Dev"
+  }
 }
 
 resource "azurerm_virtual_network" "azfw_vnet" {
@@ -20,6 +25,11 @@ resource "azurerm_virtual_network" "azfw_vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    Service     = "Ansible"
+    Environment = "Dev"
+  }
 }
 
 resource "azurerm_subnet" "firewall_subnet" {
@@ -34,6 +44,11 @@ resource "azurerm_virtual_network" "VN-Spoke" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["192.168.0.0/16"]
+
+  tags = {
+    Service     = "Ansible"
+    Environment = "Dev"
+  }
 }
 
 resource "azurerm_subnet" "SN-Workload" {

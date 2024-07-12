d65404dd4a9e1635141f8d10dec4006f57ae8560
David Yeo <david.j.yeo@outlook.com>
2024-07-12 10:24:04 +0100

    build: azure backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -31,7 +31,10 @@ terraform {
 
 
   backend "azurerm" {
-    # use_oidc         = true # Can also be set via `ARM_USE_OIDC` environment variable.
+    container_name       = "tfstate"
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "stgtfstatedjy01"
+    use_oidc             = true
     # use_azuread_auth = true
   }
   # # cloud {

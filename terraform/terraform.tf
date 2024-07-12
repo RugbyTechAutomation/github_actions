0a90b4768646b4420fd104464a505b991bcd863c
David Yeo <david.j.yeo@outlook.com>
2024-07-12 10:36:40 +0100

    build: azure backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -35,6 +35,7 @@ terraform {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "stgtfstatedjy01"
     use_oidc             = true
+    client_id            = "034c052e-a6e5-4252-9b10-df537302b2b8"
     tenant_id            = "88ef261e-b19b-4d71-9afd-cdac31a6dcda" #var.AZURE_TENANT_ID
     key                  = "terraform.tfstate"
   }

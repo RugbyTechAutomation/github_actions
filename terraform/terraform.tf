51348cbc517c04f4e16cfd546f78ce742e82cd9b
David Yeo <david.j.yeo@outlook.com>
2024-07-17 08:17:58 +0100

    build: credentials

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -35,39 +35,9 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
-    client_id            = "034c052e-a6e5-4252-9b10-df537302b2b8"
-    tenant_id            = "88ef261e-b19b-4d71-9afd-cdac31a6dcda" #var.AZURE_TENANT_ID
+    # client_id            = "034c052e-a6e5-4252-9b10-df537302b2b8"
+    # tenant_id            = "88ef261e-b19b-4d71-9afd-cdac31a6dcda"
   }
-
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-terraform-github-actions-state"
-  #   storage_account_name = "terraformgithubactions"
-  #   container_name       = "tfstate"
-  #   key                  = "terraform.tfstate"
-  #   use_oidc             = true
-  # }
-
-  # # cloud {
-  # backend "remote" {
-  #   organization = "davidjyeo"
-  #   # token        = ""
-
-  #   workspaces {
-  #     # project = "Azure"
-  #     name = "State"
-  #   }
-  # }
-
-  # backend "azurerm" {
-  #   storage_account_name = "sttfrmmgmtuks01"           #var.TFSTATE_STORAGE_ACCOUNT
-  #   container_name       = "tfstate"                   #var.TFSTATE_CONTAINER
-  #   key                  = "ansible.terraform.tfstate" #var.TFSTATE_KEY
-  #   # subscription_id      = var.SUBSCRIPTION_ID
-  #   # tenant_id            = var.TENANT_ID
-  #   resource_group_name = "rg-tfrm-mgmt-uks-01" #var.TFSTATE_RESOURCE_GROUP
-  #   # use_azuread_auth     = true
-  # }
-
 }
 
 provider "azurerm" {
